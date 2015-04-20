8dcd7274720dee7c199044a8068bf9684e12072e
zhouziqiang <zhouziqiang@hujiang.com>
2015-04-20 18:14:16 +0800

    #修改：测试

--- plugin/scripts/demo1.js
+++ plugin/scripts/demo1.js
@@ -27,6 +27,10 @@
     $.log(obj, arr, 234234, '234234');
 
 
+    function loopInvoke(){
+
+    }
+
     $.fn.pluginName = function(options){
         var defaults = {
             color: '#F50',
@@ -44,7 +48,7 @@
             $this.append(':'+title);
             $this.css({
                 color: settings.color
-            })
+            });
         });
     };
 
@@ -82,6 +86,242 @@
 
         return instance.beautify();
     };
+
+
+    var _methods = {
+            doMethod: function(){
+                console.log('method1');
+            },
+            doMethod2: function(){
+                console.log('method2');
+            }
+        },
+        methods = {
+        init: function (options) {
+
+            return this.each(function () {
+
+                var $this = $(this),
+
+                    args = $.extend({}, defaults, options),
+
+                    data = $this.data('tooltip'),
+                    tooltip = $('<div />', {
+                        text: $this.attr('title')
+                    });
+
+                console.log(data);
+
+                // If the plugin hasn't been initialized yet
+                if (!data) {
+
+                    /*
+                     Do more setup stuff here
+                     */
+
+                    $(this).data('tooltip', {
+                        target: $this,
+                        tooltip: tooltip
+                    });
+                    
+                }
+
+            });
+        },
+        publicMethod: function(){
+            _methods.doMethod();
+        },
+        destroy: function () {
+
+            return this.each(function () {
+
+                var $this = $(this),
+                    data = $this.data('tooltip');
+
+                // Namespacing FTW
+                $(window).unbind('.tooltip');
+                data.tooltip.remove();
+                $this.removeData('tooltip');
+
+            });
+
+        },
+        show: function (data) {
+            console.log('show', data);
+        },
+        hide: function () {
+            // ...
+        },
+        update: function (content) {
+            // ...
+        }
+    };
+
+    $.fn.tooltip = function (method) {
+
+        if (methods[method]) {
+            //如果存在方法，调用这个方法
+            //apply 是 obj.method(arg1, arg2, arg3) 转换成 method(obj, [arg1, arg2, arg3]) 的过程.
+            //Array.prototype.slice.call(arguments, 1), 转化成数组
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            //传进来是对象
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.tooltip');
+        }
+
+    };
+    
+
+    var Plugin2 = function(element, options){
+        this.element = element;
+        this.options = options;
+        this.init();
+    };
+
+    Plugin2.prototype = {
+        init: function(){
+            this.create();
+            console.log('init');
+        },
+        create: function(){
+            console.log(this.element)
+
+        },
+        setValue: function(key, value){
+            this.options[key] = value;
+        },
+        publicMethod: function(value){
+            console.log('publicMethod', value);
+        }
+    };
+
+    $.fn.plugin2 = function(options){
+
+        return this.each(function(){
+
+            var $this = $(this),
+                data = $this.data('plugin2');
+
+            if(!data){
+                var opts = typeof options === 'object' ? options : {};
+
+                opts = $.extend({}, $.fn.plugin2.defaults, opts);
+                
+                data = new Plugin2(this, opts);
+
+                $this.data('plugin2', data);
+            }
+
+            if(typeof options === 'string' && data[options]){
+                data[options].apply(this, arguments);
+            }
+
+        });
+    };
+
+    $.fn.plugin2.defaults = {
+        'font': '16px'
+    };
+
+
+    var subArr = [
+        {
+            'title': '专题',
+            'body': '暗色调发斯蒂芬阿斯顿发撒旦法asdf sd是否'
+        },
+        {
+            'title': '专题23',
+            'body': '暗色调发斯蒂芬阿斯顿'
+        },
+        {
+            'title': '专题234',
+            'body': '暗色调发斯蒂芬阿斯顿发撒旦'
+        },
+        {
+            'title': '专题5',
+            'body': '暗色调发斯蒂芬阿斯顿发撒旦法asdf sd是'
+        },
+        {
+            'title': '专题6',
+            'body': '暗色调发斯蒂芬阿斯顿发撒旦法asd'
+        }
+    ];
+
+    var _defaults = {
+            data: [],
+        };
+
+    var ScrollSub = function(element, options){
+        this.element = element;
+        this.options = $.extend({}, _defaults, options);
+    };
+
+    ScrollSub.prototype = {
+        init: function(){
+            var that = this,
+                $element = $(this.element);
+            
+            $element.css({
+                color: this.options.color
+            });
+            this.dataArr = [];
+            this.count = 0;
+        },
+        getArr: function(){
+            var children  = $element.children();
+
+            
+
+        },
+        createHtml: function(arr, pattern){
+
+        },
+        loop: function(){
+
+        }
+    }
+
+
+
+    var core = {
+            init: function(elem, options){
+
+                console.log('init');
+            },
+            test: function(){
+                console.log('test2_public');
+            },
+            _test2: function(){
+                console.log('test2_private');
+            }
+        };
+
+    $.fn.scrollSub = function(options){
+        //
+        var that = this;
+        this.pub = function(value){
+            console.log('calculate', value);
+            this.css('color', value);
+        };
+
+        return this.each(function(){
+
+            var $this = $(this),
+                options = $.extend({}, defaults, options);
+            // console.log(this, that);
+            core.init($this, options);
+
+            $this.css({
+                color: options.color
+            });
+
+        });
+
+    };
+
+    window.ScrollSub = ScrollSub;
 
     // window.Beautifier = Beautifier;
 
